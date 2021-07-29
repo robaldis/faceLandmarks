53814ad9faa549c848fd9f92d754f87886ef5bb8
robaldis <robertaldis@gmail.com>
2021-07-29 01:26:05 +0100

    feat: seperated out Transform into own class

--- FaceLandmarks.py
+++ FaceLandmarks.py
@@ -25,36 +25,7 @@ from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-
-class Transform():
-    def __init__(self):
-        pass
-
-    def resize(self, image, landmarks, img_size):
-        image = TF.resize(image, img_size)
-        return image, landmarks
-
-    def crop_face(self, image, landmarks, crops):
-        image = TF.to_tensor(image)
-        left = int(crops["left"])
-        top = int(crops["top"])
-        width = int(crops["width"])
-        height = int(crops["height"])
-
-        image = TF.crop(image, top, left, height, width)
-
-        img_shape = np.array(image).shape
-        print(img_shape)
-        landmarks = torch.tensor(landmarks) - torch.tensor([[left, top]])
-        landmarks = landmarks / torch.tensor([img_shape[1], img_shape[2]])
-
-        return image, landmarks
-
-    def __call__(self, image, landmarks, crops):
-        image, landmarks = self.crop_face(image, landmarks, crops)
-        image, landmarks = self.resize(image, landmarks, (224, 224))
-
-        return image, landmarks
+from transform import Transform
 
 
 class FaceLandmarkDataset(Dataset):
---
+++ transform.py
@@ -0,0 +1,38 @@
+import torch
+import torchvision.transforms.functional as TF
+import numpy as np
+
+
+# Transform: perform transforms on the dataset image
+# The use for all these transforms is to make the training of the CNN to be
+# better. This means a slight rotation, change in brightness, and crop to focus
+# on the face.
+class Transform():
+    def __init__(self):
+        pass
+
+    def resize(self, image, landmarks, img_size):
+        image = TF.resize(image, img_size)
+        return image, landmarks
+
+    def crop_face(self, image, landmarks, crops):
+        image = TF.to_tensor(image)
+        left = int(crops["left"])
+        top = int(crops["top"])
+        width = int(crops["width"])
+        height = int(crops["height"])
+
+        image = TF.crop(image, top, left, height, width)
+
+        img_shape = np.array(image).shape
+        print(img_shape)
+        landmarks = torch.tensor(landmarks) - torch.tensor([[left, top]])
+        landmarks = landmarks / torch.tensor([img_shape[1], img_shape[2]])
+
+        return image, landmarks
+
+    def __call__(self, image, landmarks, crops):
+        image, landmarks = self.crop_face(image, landmarks, crops)
+        image, landmarks = self.resize(image, landmarks, (224, 224))
+
+        return image, landmarks
