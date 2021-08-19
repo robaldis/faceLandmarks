3e9ad07756f1ab8eb18b05a2ebd4a688a1c9d9ae
robaldis <robertaldis@gmail.com>
2021-08-19 11:33:13 +0100

    feat: testing other training models

--- moduleTesting.py
+++ moduleTesting.py
@@ -3,10 +3,10 @@
 import torch.nn as nn
 import torch.optim as optim
 
-from training.his import train
-from network.his import Network
-from dataobject.his import FaceLandmarksDataset, split_dataset
-from transform.his import Transforms
+from training.me import train
+from network.me import Network
+from dataobject.me import FaceLandmarksDataset, split_dataset
+from transform.me import Transforms
 
 
 dataset = FaceLandmarksDataset(Transforms())
@@ -17,6 +17,6 @@ criterian = nn.MSELoss()
 optimizer = optim.Adam(network.parameters(), lr=0.0001)
 train_loader, valid_loader = split_dataset(dataset)
 
-train(network, criterian, optimizer, train_loader, valid_loader, epoch=20)
+train(network, criterian, optimizer, train_loader, valid_loader, epoch=50)
 
 
